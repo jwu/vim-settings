35fd4d3f84482fdef50b9e1fb15115ec7a963675
Johnny Wu <wujie@xmfunny.com>
2024-05-31 20:37:20 +0800

    fix search compl bugs

--- init.lua
+++ init.lua
@@ -1010,16 +1010,17 @@ require('lazy').setup({
         })
       }
 
+      -- DELME: disable cause it will make cursor jump amongs multiple windows
       -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        view = {
-          entries = { name = 'wildmenu', separator = ' | ' }
-        },
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+      -- cmp.setup.cmdline({ '/', '?' }, {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   view = {
+      --     entries = { name = 'wildmenu', separator = ' | ' }
+      --   },
+      --   sources = {
+      --     { name = 'buffer' }
+      --   }
+      -- })
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
